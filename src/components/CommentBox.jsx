cb3e6c6bfac6e6090e1443b15886569d324a833a
scloudyy <onecloud.shen@gmail.com>
2016-05-16 22:32:51 +0800

    use backdoor to marked

--- src/components/CommentBox.jsx
+++ src/components/CommentBox.jsx
@@ -1,14 +1,54 @@
-var React = require('react');
+const React = require('react');
+const marked = require('marked');
 
-var CommentBox = React.createClass({
+let CommentBox = React.createClass({
 	render: function() {
 		return (
-      <div>
-        <ul>
-          1122ss
-        </ul>
-      </div>
+      <div className="commentBox">
+				<h1>commentList</h1>
+				<CommentList/>
+				<CommentForm/>
+			</div>
     );
+	}
+});
+
+let CommentList = React.createClass({
+	render: function() {
+		return (
+			<div className="commentList">
+				<Comment author="Peter">This is one comment</Comment><br/>
+				<Comment author="Tom">This is *anothor* comment</Comment><br/>
+			</div>
+		);
+	}
+});
+
+let CommentForm = React.createClass({
+	render: function() {
+		return (
+			<div className="commentForm">
+				commentForm
+			</div>
+		);
+	}
+});
+
+let Comment = React.createClass({
+	rawMarkup: function() {
+		let rawMarkup = marked(this.props.children.toString(), {sanitize: true});
+		return { __html: rawMarkup };
+	},
+
+	render: function () {
+		return (
+			<div className="comment">
+				<h2 className="commentAuthor">
+					{this.props.author}
+				</h2>
+				<span dangerouslySetInnerHTML={this.rawMarkup()} />
+			</div>
+		);
 	}
 });
 
